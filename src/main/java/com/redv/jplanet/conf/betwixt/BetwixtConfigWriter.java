52d85d7ffed35cda9ddec40552b8cea169d6fa2e
zhoushuqun <zhoushuqun@0f83c0aa-0232-0410-92b9-556d6036b9be>
2007-06-11 18:23:08 +0000

    Synchronized write config.

--- src/main/java/com/redv/jplanet/conf/betwixt/BetwixtConfigWriter.java
+++ src/main/java/com/redv/jplanet/conf/betwixt/BetwixtConfigWriter.java
@@ -25,7 +25,7 @@ public class BetwixtConfigWriter implements ConfigWriter {
 	 * 
 	 * @see com.redv.jplanet.conf.ConfigWriter#write(com.redv.jplanet.Planet)
 	 */
-	public void write(Planet planet) throws Exception {
+	public synchronized void write(Planet planet) throws Exception {
 		Writer output = new OutputStreamWriter(new FileOutputStream(new File(
 				Constant.getDataDir(), "jplanet.xml")), "UTF-8");
 		output.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
