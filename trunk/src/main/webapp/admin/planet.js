35c59f1e864ab67c422af436f0305e8edf3c7495
zhoushuqun <zhoushuqun@0f83c0aa-0232-0410-92b9-556d6036b9be>
2007-06-21 11:56:37 +0000

    Beautiful admin page and optimize the javascript.

--- trunk/src/main/webapp/admin/planet.js
+++ trunk/src/main/webapp/admin/planet.js
@@ -4,42 +4,32 @@ function addOpenIdRow(){
 	var td1 = document.createElement("td");
 	var input1 = document.createElement("input");
 	input1.name = "editor.openid";
+	input1.type = "text";
 	input1.className = "text";
-	input1.size = "100"
+	input1.setAttribute("style", "width:100%;");
 	td1.appendChild(input1);
 	row.appendChild(td1);
 	tbody.appendChild(row);
 }
+
 function addSubscriptionRow() {
 	var tbody = document.getElementById("subscriptions");
 	var row = document.createElement("tr");
-	var td1 = document.createElement("td");
-	var td2 = document.createElement("td");
-	var td3 = document.createElement("td");
-	var td4 = document.createElement("td");
-	var input1 = document.createElement("input");
-	var input2 = document.createElement("input");
-	var input3 = document.createElement("input");
-	var input4 = document.createElement("input");
-	input1.name = "subscription.title";
-	input2.name = "subscription.feedUrl";
-	input3.name = "subscription.siteUrl";
-	input4.name = "subscription.description";
-	input1.className = "text";
-	input2.className = "text";
-	input3.className = "text";
-	input4.className = "text";
-	input1.size = "15";
-	input2.size = "30";
-	input3.size = "30";
-	input4.size = "40";
-	td1.appendChild(input1);
-	td2.appendChild(input2);
-	td3.appendChild(input3);
-	td4.appendChild(input4);
-	row.appendChild(td1);
-	row.appendChild(td2);
-	row.appendChild(td3);
-	row.appendChild(td4);
+
+	var tds = new Array();
+	var inputs = new Array();
+	for (var i = 0; i < 4; i++) {
+		tds[i] = document.createElement("td");
+		inputs[i] = document.createElement("input");
+		inputs[i].className = "text";
+		inputs[i].setAttribute("style", "width:100%;");
+		tds[i].appendChild(inputs[i]);
+		row.appendChild(tds[i]);
+	}
 	tbody.appendChild(row);
+
+	inputs[0].name = "subscription.title";
+	inputs[1].name = "subscription.feedUrl";
+	inputs[2].name = "subscription.siteUrl";
+	inputs[3].name = "subscription.description";
 }
